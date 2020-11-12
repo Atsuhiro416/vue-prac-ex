60d5e2fd0afe5bb8484a5eda738cc3ae5e59affe
Atsuhiro416 <myamp012@gmail.com>
2020-11-12 14:45:01 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <input type="text" v-model="zipCode">
+    <button @click="inputAddress">住所自動入力</button>
+    <p>Address：{{ showAddress }}</p>
     <router-view/>
   </div>
 </template>
 
+// <script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      zipCode: "",
+      allAddress: "",
+      showAddress: ""
+    }
+  },
+
+  async created() {
+    let item = await axios.get(
+      `https://apis.postcode-jp.com/api/v4/postcodes/${this.zipCode}?apikey=zabUzil9V6V3E2UG9HKDZ3WJfsx1QbRyyrc1sOC`,
+      // {
+      //   params: {
+      //   apiKey: "zabUzil9V6V3E2UG9HKDZ3WJfsx1QbRyyrc1sOC"
+      //   }
+      // }
+    );
+
+    console.log(item);
+
+    this.data = item.data
+    this.allAddress = this.data.data[0].allAddress;
+  },
+
+  methods: {
+    inputAddress() {
+      this.showAddress = this.allAddress;
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
